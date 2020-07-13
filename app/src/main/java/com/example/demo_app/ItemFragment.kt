2b8d257e273961c4d9ad63bf44b9c593b4e5be6e
Tina <kviditch@gmail.com>
2020-07-13 23:54:43 +1000

    add list Item click

--- app/src/main/java/com/example/demo_app/ItemFragment.kt
+++ app/src/main/java/com/example/demo_app/ItemFragment.kt
@@ -1,13 +1,14 @@
 package com.example.demo_app
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 /**
  * A fragment representing a list of Items.
@@ -16,6 +17,9 @@ class ItemFragment : Fragment() {
 
     private var columnCount = 1
 
+    interface onItemClickListener {
+        fun selectItem(s: String?)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +58,9 @@ class ItemFragment : Fragment() {
                 }
 
                // viewAdapter = MyItemRecyclerViewAdapter(myDataset)
-                view.adapter = MyItemRecyclerViewAdapter(allData)
+                view.adapter = MyItemRecyclerViewAdapter(allData) {
+                        Toast.makeText(this.context, "click", Toast.LENGTH_SHORT).show()
+                }
             }
         }
         return view
--- app/src/main/java/com/example/demo_app/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/demo_app/MyItemRecyclerViewAdapter.kt
@@ -1,23 +1,29 @@
 package com.example.demo_app
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 
 import com.example.demo_app.dummy.DummyContent.DummyItem
+import kotlin.properties.Delegates
 
 /**
  * [RecyclerView.Adapter] that can display a [DummyItem].
  * TODO: Replace the implementation with code for your data type.
  */
 class MyItemRecyclerViewAdapter(
-        private val values: DataModel
+        private val values: DataModel, private val listener: (Data) -> Unit
 )
     : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
 
     private val dataset = values
+    private var selectedPos = -1
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -30,6 +36,16 @@ class MyItemRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = dataset.getData()[position]
         holder.idView.text = item.getName()
+        holder.itemView.setOnClickListener {
+            listener(item)
+            selectedPos = position
+            notifyItemChanged(position)
+
+        }
+
+        if (selectedPos == position) {
+            holder.itemView.setBackgroundColor(Color.BLUE)
+        }
     }
 
     override fun getItemCount(): Int = dataset.getSize()
